e7ed3a22843730291ebe1a1b9b62ef9011b8ed39
Philippe chambon <joan_krugg@live.fr>
2016-11-11 15:54:27 +0100

    set for front

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,40 @@
+class DosesController < ApplicationController
+before_action :set_dose, only: [:edit, :update, :destroy]
+
+ def create
+   @cocktail = Cocktail.find(params[:cocktail_id])
+   @dose = @cocktail.doses.new(dose_params)
+   if @dose.save
+     redirect_to @cocktail
+   else
+     render 'cocktails/show'
+   end
+ end
+
+ def edit
+   @cocktail = @dose.cocktail
+ end
+
+ def update
+   if @dose.update(dose_params)
+     redirect_to @dose.cocktail
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+   @dose.destroy
+   redirect_to @dose.cocktail
+ end
+
+ private
+
+ def set_dose
+   @dose = Dose.find(params[:id])
+ end
+
+ def dose_params
+   params.require(:dose).permit(:ingredient_id, :description)
+ end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,27 @@
+Cocktail.destroy_all
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody Marry")
+Cocktail.create(name: "Sex on the beach")
+Cocktail.create(name: "Cuba Libre")
+Cocktail.create(name: "White Russian")
+Cocktail.create(name: "Long Island Ice Tea")
+
+
+Ingredient.destroy_all
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)["drinks"]
+ingredients.map! { |ingredient| { name: ingredient["strIngredient1"] } }
+Ingredient.create(ingredients)
+
+
+
+
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
